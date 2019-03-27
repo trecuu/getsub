f1bdae8c73d208f941a5232ab2c736249fea712c
trecuu <ricardoperazza@hotmail.com>
2019-03-27 03:38:37 -0300

    Update getsub.py

--- getsub.py
+++ getsub.py
@@ -10,13 +10,13 @@ videoFile = str(sys.argv[1]) #file location ex.: C:\Downloads\Video.mkv
 
 
 def get_hash(name):
-        readsize = 64 * 1024
-        with open(name, 'rb') as f:
-            size = os.path.getsize(name)
-            data = f.read(readsize)
-            f.seek(-readsize, os.SEEK_END)
-            data += f.read(readsize)
-        return hashlib.md5(data).hexdigest()
+	readsize = 64 * 1024
+	with open(name, 'rb') as f:
+		size = os.path.getsize(name)
+		data = f.read(readsize)
+		f.seek(-readsize, os.SEEK_END)
+		data += f.read(readsize)
+	return hashlib.md5(data).hexdigest()
 
 
 def get_sub(fileHash, language):
